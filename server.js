f99468b3c0850bdab218636e89b84da00b38eb9a
Mohd5zan <mohd5zan77@gmail.com>
2026-08-19 13:13:01 +0530

    first commit till auth

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-const 
+require("dotenv").config();
+const app = require("./src/app");
+const connectdb = require("./src/db/db");
+connectdb();
+app.listen(process.env.PORT, () => {
+  console.log(`Server started at ${process.env.PORT}`);
+});
---
+++ src/db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+async function connectdb() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database Connected Successfully");
+  } catch (err) {
+    console.error("Database Connection Error:", err);
+  }
+}
+module.exports = connectdb;
