8263b9ee3983dd07ef6d55300c03c00f7b27918b
Mohd5zan <mohd5zan77@gmail.com>
2026-08-19 18:35:59 +0530

    till album creation

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,3 +1,4 @@
+const albumModel = require("../models/album.model");
 const musicModel = require("../models/music.model");
 const { uploadFile } = require("../services/storage.services");
 const jwt = require("jsonwebtoken");
@@ -39,4 +40,39 @@ async function createMusic(req, res) {
     });
   }
 }
-module.exports = { createMusic };
+
+async function createAlbum(req, res) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401) .json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role !== "artist") {
+      return res
+        .status(403)
+        .json({ message: "You dont have access to create an album" });
+    }
+    const { title, musics } = req.body;
+    const album = await albumModel.create({
+      title,
+      artist: decoded.id,
+      music: musics,
+    });
+    res.status(201).json({
+      message: "Album created successfully",
+      album: {
+        id: album._id,
+        title: album.title,
+        artist: album.aritst,
+        musics: album.musics,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+}
+module.exports = { createMusic, createAlbum };
--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,14 +1,15 @@
-const express=require('express');
-const {createMusic} =require('../controllers/music.controller');
-const multer=require('multer');
+const express = require("express");
+const { createMusic, createAlbum } = require("../controllers/music.controller");
+const multer = require("multer");
+const { authArtist } = require("../middlewares/authmiddleware");
 
-const upload=multer({
-    storage:multer.memoryStorage();
-})
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
-const router=express.Router();
+const router = express.Router();
 
-router.post('/upload',upload.single("music"),createMusic)
+router.post("/upload", authArtist, upload.single("music"), createMusic);
+router.post("/album", authArtist,  createAlbum);
 
-
-module.exports=router;
+module.exports = router;
